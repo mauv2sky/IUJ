7c6e25607b00174e4396368f58ebd4bbd7b8a087
cmzlsho@gmail.com <cmzlsho@gmail.com>
2023-03-20 17:55:12 +0900

    ✨add 지도에서 사용하는 dto

---
+++ backend/src/main/java/com/iuj/backend/api/domain/dto/common/BoundDto.java
@@ -0,0 +1,6 @@
+package com.iuj.backend.api.domain.dto.common;
+
+public class BoundDto {
+    String[] sw;
+    String[] ne;
+}
---
+++ backend/src/main/java/com/iuj/backend/api/domain/dto/response/AverageDeal.java
@@ -0,0 +1,10 @@
+package com.iuj.backend.api.domain.dto.response;
+
+import com.iuj.backend.api.domain.enums.DealType;
+
+public class AverageDeal {
+    DealType deal_type;
+    int guarantee;
+    int price;
+    int monthly;
+}
