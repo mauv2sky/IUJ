1708c1aa5be006319363cab34b57a8d780dd6910
김빛누리 <bnurik104@gmail.com>
2023-03-24 02:28:00 +0900

    ✨지도 api 지도레벨 구분 기능 - 레벨 구분해서 다른 data return - DealType.java에서 잘못된 implments (존재하지 않는 인터페이스 상속) 로 발생한 에러 수정

--- backend/src/main/java/com/iuj/backend/api/domain/enums/DealType.java
+++ backend/src/main/java/com/iuj/backend/api/domain/enums/DealType.java
@@ -1,23 +1,17 @@
 package com.iuj.backend.api.domain.enums;
 
-import com.iuj.backend.api.domain.converter.BaseEnumCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.util.EnumSet;
 
 @Getter
 @AllArgsConstructor
-public enum DealType implements BaseEnumCode<String> {
+public enum DealType {
     BUY("매매"),
     LONG_TERM_RENT("전세"),
     MONTHLY_RENT("월세");
 
     private final String name;
-
-    @Override
-    public String getValue() {
-        return this.getName();
-    }
 
     public static DealType findByName(String name){
         return EnumSet.allOf(DealType.class).stream()
--- backend/src/main/java/com/iuj/backend/api/service/BuildingService.java
+++ backend/src/main/java/com/iuj/backend/api/service/BuildingService.java
@@ -1,9 +1,9 @@
 package com.iuj.backend.api.service;
 
 import com.iuj.backend.api.domain.dto.common.BoundDto;
+import com.iuj.backend.api.domain.dto.mapping.LocationMapping;
 import com.iuj.backend.api.domain.dto.request.PlaceMainRequest;
 import com.iuj.backend.api.domain.dto.response.BuildingDto;
-import com.iuj.backend.api.domain.entity.building.Apt;
 import com.iuj.backend.api.domain.enums.BuildingType;
 import com.iuj.backend.api.repository.building.AptRepository;
 import com.iuj.backend.api.repository.building.JDBCBuildingRepository;
@@ -11,8 +11,6 @@ import com.iuj.backend.api.repository.building.OfficetelRepository;
 import com.iuj.backend.api.repository.building.VillaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,16 +24,44 @@ public class BuildingService {
     private final JDBCBuildingRepository jdbcBuildingRepository;
 
     public List<BuildingDto> getBuildingList(PlaceMainRequest request){
-        BoundDto bound = request.getBound();
-        List<BuildingDto> buildingList = jdbcBuildingRepository.getBuildingList(bound.getSw(), bound.getNe(), request.getDeal_type(), request.getType(), request.getFilter());
         BuildingType buildingType = request.getType();
+        BoundDto bound = request.getBound();
+        int level = request.getLevel();
 
-        for(BuildingDto building : buildingList){
-            building.setType(buildingType);
+        List<BuildingDto> buildingList = null;
+
+        if(level > 9) {
+            throw new IllegalArgumentException();
+        } else if(level > 3){
+            List<LocationMapping> locList = null;
+
+            if(buildingType.equals(BuildingType.APT)) {
+                locList = aptRepository.getByBound(bound.getSw()[0], bound.getSw()[1], bound.getNe()[0], bound.getNe()[1]);
+            } else if(buildingType.equals(BuildingType.OFFICETEL)){
+                locList = officetelRepository.getByBound(bound.getSw()[0], bound.getSw()[1], bound.getNe()[0], bound.getNe()[1]);
+            } else {
+                locList = villaRepository.getByBound(bound.getSw()[0], bound.getSw()[1], bound.getNe()[0], bound.getNe()[1]);
+            }
+
+            buildingList = locList.stream()
+                    .map(e -> BuildingDto.builder()
+                            .id(e.getId())
+                            .name(e.getName())
+                            .latlng(new Double[]{e.getLat(), e.getLng()})
+                            .build())
+                    .collect(Collectors.toList());
+        } else {
+            buildingList = jdbcBuildingRepository.getBuildingList(bound.getSw(), bound.getNe(), request.getDeal_type(), request.getType(), request.getFilter());
+            buildingList = buildingList.stream()
+                    .filter(t -> t.getAverageDeal().getDeal_type() != null)
+                    .collect(Collectors.toList());
         }
 
-        return buildingList.stream()
-                .filter(t -> t.getAverageDeal().getDeal_type() != null)
-                .collect(Collectors.toList());
+        if(buildingList != null){
+            for(BuildingDto building : buildingList){
+                building.setType(buildingType);
+            }
+        }
+        return buildingList;
     }
 }
