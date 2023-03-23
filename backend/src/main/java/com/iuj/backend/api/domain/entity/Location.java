987d64e5e39f3a990d9b7e84198c2f45741cb815
정재훈 <wogns8123@naver.com>
2023-03-23 16:41:59 +0900

    Revert "Merge branch 'back' into 'main'"
    This reverts merge request !49

--- backend/src/main/java/com/iuj/backend/api/domain/entity/Location.java
+++ backend/src/main/java/com/iuj/backend/api/domain/entity/Location.java
@@ -1,9 +1,6 @@
 package com.iuj.backend.api.domain.entity;
 
-import com.iuj.backend.api.domain.entity.building.Apt;
-import com.iuj.backend.api.domain.entity.building.AptDeal;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -12,16 +9,10 @@ import javax.persistence.MappedSuperclass;
 @Getter
 @Setter
 @MappedSuperclass
-@NoArgsConstructor
 public abstract class Location {
     @Column(length=45)
     private String lat;
 
     @Column(length=45)
     private String lng;
-
-    public Location(Apt entity) {
-        this.lat = entity.getLat();
-        this.lng = entity.getLng();
-    }
 }
