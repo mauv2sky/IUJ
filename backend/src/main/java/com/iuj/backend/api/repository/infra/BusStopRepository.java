8ced14b54a165d4ea02422fdf8bec0aac57eb681
정재훈 <wogns8123@naver.com>
2023-03-23 18:05:23 +0900

    버스 정류장

--- backend/src/main/java/com/iuj/backend/api/repository/infra/BusStopRepository.java
+++ backend/src/main/java/com/iuj/backend/api/repository/infra/BusStopRepository.java
@@ -2,10 +2,13 @@ package com.iuj.backend.api.repository.infra;
 
 import com.iuj.backend.api.domain.entity.infra.BusStop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface BusStopRepository extends JpaRepository<BusStop, Integer> {
     List<BusStop> findAll();
+    @Query("select b from BusStop b where b.lat > :minLatitude and b.lat < :maxLatitude and b.lng > :minLongitude and b.lng < :maxLongitude")
     List<BusStop> findAllBusBtwlngAndlat(double minLatitude, double maxLatitude, double minLongitude, double maxLongitude);
 }
