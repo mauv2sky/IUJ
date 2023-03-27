d0feaa15ed1db14ff6546e2165c516ce78ff89ae
hossuny <cook8172@naver.com>
2023-03-27 14:24:50 +0900

    ✨icon color

--- frontend/src/components/InfraIcon/InfraIconamenities.tsx
+++ frontend/src/components/InfraIcon/InfraIconamenities.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './InfraIconamenities.module.scss';
-import { FaHospitalUser } from 'react-icons/fa';
+import { MdLocalHospital } from 'react-icons/md';
+import { BsFillBasket3Fill } from 'react-icons/bs';
+import { MdLocalConvenienceStore } from 'react-icons/md';
 
 function InfraIconamenities() {
-  return <div>InfraIconamenities</div>;
+  const [selectedBtn, setSelectedBtn] = useState('');
+
+  /** 어린이집 버튼 클릭 시 */
+  const onClickHospitalBtn = () => {
+    console.log('병원 위치 요청');
+    setSelectedBtn(selectedBtn === 'hospital' ? '' : 'hospital');
+  };
+  /** 유치원 버튼 클릭 시 */
+  const onClickMartBtn = () => {
+    console.log('마트 위치 요청');
+    setSelectedBtn(selectedBtn === 'mart' ? '' : 'mart');
+  };
+  /** 초등학교 버튼 클릭 시 */
+  const onClickConvenienceStoreBtn = () => {
+    console.log('편의점 위치 요청');
+    setSelectedBtn(selectedBtn === 'conveniencestore' ? '' : 'conveniencestore');
+  };
+  return (
+    <div className={styles.component}>
+      <div className={styles.iconall}>
+        <div className={`${styles.iconborder} ${selectedBtn === 'hospital' ? styles.selected : ''}`} onClick={onClickHospitalBtn}>
+          <span className={styles.icon}>
+            <MdLocalHospital />
+          </span>
+        </div>
+        <p>병원</p>
+      </div>
+      <div className={styles.iconall}>
+        <div className={`${styles.iconborder} ${selectedBtn === 'mart' ? styles.selected : ''}`} onClick={onClickMartBtn}>
+          <span className={styles.icon}>
+            <BsFillBasket3Fill />
+          </span>
+        </div>
+        <p>마트</p>
+      </div>
+      <div className={styles.iconall}>
+        <div className={`${styles.iconborder} ${selectedBtn === 'conveniencestore' ? styles.selected : ''}`} onClick={onClickConvenienceStoreBtn}>
+          <span className={styles.icon}>
+            <MdLocalConvenienceStore />
+          </span>
+        </div>
+        <p>편의점</p>
+      </div>
+    </div>
+  );
 }
 
 export default InfraIconamenities;
--- frontend/src/components/InfraIcon/InfraIconcultures.tsx
+++ frontend/src/components/InfraIcon/InfraIconcultures.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './InfraIconcultures.module.scss';
 import { MdMovie } from 'react-icons/md';
+import { MdOutlinePark } from 'react-icons/md';
 
 function InfraIconcultures() {
-  return <div>InfraIconcultures</div>;
+  const [selectedBtn, setSelectedBtn] = useState('');
+
+  /** 공원 버튼 클릭 시 */
+  const onClickParkBtn = () => {
+    console.log('공원 위치 요청');
+    setSelectedBtn(selectedBtn === 'park' ? '' : 'park');
+  };
+  /** 영화관 버튼 클릭 시 */
+  const onClickMovieBtn = () => {
+    console.log('영화관 위치 요청');
+    setSelectedBtn(selectedBtn === 'movie' ? '' : 'movie');
+  };
+  return (
+    <div className={styles.component}>
+      <div className={styles.iconall}>
+        <div className={`${styles.iconborder} ${selectedBtn === 'park' ? styles.selected : ''}`} onClick={onClickParkBtn}>
+          <span className={styles.icon}>
+            <MdOutlinePark />
+          </span>
+        </div>
+        <p>공원</p>
+      </div>
+      <div className={styles.iconall}>
+        <div className={`${styles.iconborder} ${selectedBtn === 'movie' ? styles.selected : ''}`} onClick={onClickMovieBtn}>
+          <span className={styles.icon}>
+            <MdMovie />
+          </span>
+        </div>
+        <p>영화관</p>
+      </div>
+    </div>
+  );
 }
 
 export default InfraIconcultures;
