b4d0d24675925fd3205e1bd9c1094d241a1ff3e0
정재훈 <wogns8123@naver.com>
2023-03-27 18:00:42 +0900

    상세 페이지 API

--- backend/src/main/java/com/iuj/backend/api/controller/DetailController.java
+++ backend/src/main/java/com/iuj/backend/api/controller/DetailController.java
@@ -28,7 +28,9 @@ public class DetailController {
     private final AcademyService academyService;
     private final SafeService safeService;
     private final ConviService conviService;
+    private final ConviService HospitalService;
     private final CultureService cultureService;
+
     @GetMapping("/apt/{id}")
     @ApiOperation(value="아파트 상세페이지 정보", notes="아파트 상세페이지 정보")
     public Map<String, Object> getApart(@PathVariable Long id) {
@@ -38,6 +40,7 @@ public class DetailController {
         Map<String, Object> cultureMap = new HashMap<>();
         AptDto apartDTO = aptService.getApartById(id);
         List<AptDealTypeDto> aptDealTypeDTO = aptService.getDealByApartId(id);
+
 //      교통
         List<BusStopDto> busStopDTO = trafficService.findNearbyBusStops(apartDTO.getLat(), apartDTO.getLng());
         List<SubwayDto> subwayDTO = trafficService.findNearbySubways(apartDTO.getLat(), apartDTO.getLng());
@@ -51,6 +54,7 @@ public class DetailController {
 
 //        편의시설
         List<ConviDto> conviDto = conviService.findNearbyConvi(apartDTO.getLat(), apartDTO.getLng());
+        List<HospitalDto> hospitalDto = conviService.findNearbyHospital(apartDTO.getLat(), apartDTO.getLng());
 
 //        문화시설
         List<CinemaDto> cinemaDto = cultureService.findNearbyCinemas(apartDTO.getLat(), apartDTO.getLng());
@@ -59,21 +63,24 @@ public class DetailController {
         List<ParkDto> parkDto = cultureService.findNearbyParks(apartDTO.getLat(), apartDTO.getLng());
 
 
+
         resultMap.put("apart", apartDTO);
         resultMap.put("deal", aptDealTypeDTO);
 //      교통 추가
-//        trafficMap.put("bus", busStopDTO);
-//        trafficMap.put("subway", subwayDTO);
-//        resultMap.put("traffic", trafficMap);
+        trafficMap.put("bus", busStopDTO);
+        trafficMap.put("subway", subwayDTO);
+        resultMap.put("traffic", trafficMap);
 //      학군 추가
-//        resultMap.put("school", schoolDto);
-//        resultMap.put("academy", academyDto);
+        resultMap.put("school", schoolDto);
+        resultMap.put("academy", academyDto);
 //      치안 추가
-//        safeMap.put("CCTV", cctvDto);
-//        safeMap.put("Police", policeDto);
-//
-//        resultMap.put("편의점", conviDto);
-//        resultMap.put("safe", safeMap);
+        safeMap.put("CCTV", cctvDto);
+        safeMap.put("Police", policeDto);
+        resultMap.put("safe", safeMap);
+//      편의 시설 추가
+        resultMap.put("편의점", conviDto);
+        resultMap.put("병원", hospitalDto);
+
 
         cultureMap.put("영화관", cinemaDto);
         cultureMap.put("도서관", libraryDto);
--- backend/src/main/java/com/iuj/backend/api/domain/dto/response/HospitalDto.java
+++ backend/src/main/java/com/iuj/backend/api/domain/dto/response/HospitalDto.java
@@ -5,21 +5,17 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class LibraryDto {
+public class HospitalDto {
     private int id;
     private String name;
     private String lat;
     private String lng;
-    private String addr;
-    private String type;
 
-    public LibraryDto(int id, String name, String lat, String lng, String addr, String type){
+    public HospitalDto(int id, String name, String lat, String lng){
         this.id = id;
         this.name = name;
         this.lat = lat;
         this.lng = lng;
-        this.addr = addr;
-        this.type = type;
     }
 
 }
