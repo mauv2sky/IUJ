03f6326513045691f6e60003360503213af54301
박준수 <qwnstn@naver.com>
2023-03-30 11:23:42 +0900

    ✨ 관심매물, 선호필터에 jwt 적용

--- backend/src/main/java/com/iuj/backend/api/controller/RecommController.java
+++ backend/src/main/java/com/iuj/backend/api/controller/RecommController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/recomm")
@@ -21,9 +23,9 @@ public class RecommController {
 
     @PostMapping("")
     @ApiOperation(value = "선호 필터 ", notes = "선호 필터 등록 api")
-    public ResponseEntity<Object> addRecomm(@RequestBody RecommRequest request, @RequestHeader("X-Auth-Token") String authToken) {
+    public ResponseEntity<Object> addRecomm(@RequestBody RecommRequest request, Principal principal) {
         try {
-            recommService.addRecomm(request, authToken);
+            recommService.addRecomm(request, principal.getName());
             return ResponseEntity.ok().build();
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred: " + e.getMessage());
@@ -34,10 +36,9 @@ public class RecommController {
 
     @DeleteMapping("")
     @ApiOperation(value = "선호 필터 삭제 api", notes = "선호 필터 삭제 api")
-    public ResponseEntity<Object> delRecomm(@RequestBody DelRecommRequest request,
-                                          @RequestHeader(value="X-Auth-Token") String authToken) {
+    public ResponseEntity<Object> delRecomm(@RequestBody DelRecommRequest request, Principal principal) {
         try{
-            recommService.delRecomm(request, authToken);
+            recommService.delRecomm(request, principal.getName());
             return ResponseEntity.ok().build();
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred: " + e.getMessage());
@@ -47,9 +48,9 @@ public class RecommController {
 
     @GetMapping("")
     @ApiOperation(value = "선호 필터 조회 api", notes = "선호 필터 조회 api")
-    public ResponseEntity<Object> getAllLikesByEmail(@RequestHeader(value="X-Auth-Token") String authToken) {
+    public ResponseEntity<Object> getAllLikesByEmail(Principal principal) {
         try{
-            return new ResponseEntity<>(recommService.getAllRecommsByEmail(authToken), HttpStatus.OK);
+            return new ResponseEntity<>(recommService.getAllRecommsByEmail(principal.getName()), HttpStatus.OK);
         }catch (Exception e){
             throw new CustomException(ErrorCode.UNKNOWN_ERROR);
         }
