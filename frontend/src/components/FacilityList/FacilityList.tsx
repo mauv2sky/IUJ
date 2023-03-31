b6eda5be2ad1949c4818d341a3aa8cedb15b1a46
hossuny <cook8172@naver.com>
2023-03-31 17:58:51 +0900

    ✨ 딜 데이터 프롭스 개선

--- frontend/src/components/FacilityList/FacilityList.tsx
+++ frontend/src/components/FacilityList/FacilityList.tsx
@@ -1,39 +1,15 @@
-// import React, { useState } from 'react';
-// import FacilityListItem from '../FacilityListItem/FacilityListItem';
-// import styles from './FacilityList.module.scss';
-
-// export type FacilityType = {
-//   [key: string]: string | number[] | number;
-// };
-
-// type FacilityPropsType = {
-//   facilitylist: FacilityType[];
-// };
-
-// function FacilityList({ facilitylist }: FacilityPropsType) {
-//   return (
-//     <div>
-//       {facilitylist.map((item, index) => (
-//         <div key={index}>
-//           <div>이름: {item.name}</div>
-//           <div>주소: {item.address}</div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default FacilityList;
-
 import React, { useState } from 'react';
 import FacilityListItem, { FacilityListItemType } from '../FacilityListItem/FacilityListItem';
 import styles from './FacilityList.module.scss';
 
 export type FacilityType = {
-  [key: string]: string | string | number[];
+  [key: string]: number | string | string | string | string | string;
+  // id: number;
   // name: string;
-  // address: string;
-  // latlng: number[];
+  // type: string;
+  // lat: string;
+  // lng: string;
+  // addr: string;
 };
 
 type FacilityPropsType = {
@@ -42,18 +18,23 @@ type FacilityPropsType = {
 
 function FacilityList({ facilitylist }: FacilityPropsType) {
   const facilitylistitem: FacilityListItemType = {
+    id: 0,
     name: '',
-    address: '',
-    latlng: [0, 0],
+    type: '',
+    lat: '',
+    lng: '',
+    addr: '',
   };
 
   return (
     <div className={styles.component}>
-      {facilitylist.map((item, index) => (
-        <div key={index}>
-          <FacilityListItem facilitylistitem={item} />
-        </div>
-      ))}
+      <div>
+        {facilitylist.map((item, index) => (
+          <div key={index}>
+            <FacilityListItem facilitylistitem={item} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
