eab61201f09decd60fa663bd9885fb78ae735e18
hossuny <cook8172@naver.com>
2023-04-05 04:00:52 +0900

    ✨ 딜 리스트 페이지네이션

--- frontend/src/components/DealData/DealData.tsx
+++ frontend/src/components/DealData/DealData.tsx
@@ -1,64 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import styles from './DealData.module.scss';
-// import DealDataItem from '../DealDataItem/DealDataItem';
-// import { useAppDispatch, useAppSelector } from '../../store/hooks';
-// import { getdetailContainerState } from '../../store/slices/detailContainerSlice';
-// import { DetailPropsType, DetailType } from '../DetailInformation/DetailInformation';
-// import DealChart from '../DealChart/DealChart';
-// import axios from 'axios';
-
-// /** APIURL */
-// const APIURL = 'http://localhost:5000';
-
-// function DealData(detaillist: DetailPropsType) {
-//   const dealRelist = detaillist.detailRelist.Deal;
-//   const [dealChartlist, setDealChartlist] = useState<any>();
-
-//   useEffect(() => {
-//     axios({
-//       method: 'get',
-//       // url: APIURL + `/api/place/${props.type}/${props.id}`,
-//       url: APIURL + `/api/place/APT/9/graph`,
-//     })
-//       .then((response) => {
-//         // console.log('데이터 전송 성공이구아나');
-//         // console.log(response.data);
-//         setDealChartlist(response.data);
-//       })
-//       .catch((error) => {
-//         // console.error('데이터 전송 실패이구아나');
-//         console.error(error);
-//       });
-//   }, []);
-
-//   return (
-//     <div className={styles.component}>
-//       <div>
-//         <DealChart />
-//       </div>
-//       <div className={styles.dealtitle}>
-//         <div className={styles.type}>거래타입</div>
-//         <div className={styles.time}>거래일</div>
-//         <div className={styles.space}>전용면적</div>
-//         <div className={styles.floor}>층</div>
-//         <div className={styles.cost}>금액 (만원)</div>
-//       </div>
-//       <div>
-//         {dealRelist &&
-//           dealRelist.map((data: any, index: number) => (
-//             <div key={index}>
-//               {data.deals.map((item: any, index: number) => (
-//                 <DealDataItem key={index} dealDataItem={item} />
-//               ))}
-//             </div>
-//           ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default DealData;
-
 import React, { useState, useEffect } from 'react';
 import styles from './DealData.module.scss';
 import DealDataItem from '../DealDataItem/DealDataItem';
@@ -67,23 +6,24 @@ import { getdetailContainerState } from '../../store/slices/detailContainerSlice
 import { DetailPropsType, DetailType } from '../DetailInformation/DetailInformation';
 import DealChart from '../DealChart/DealChart';
 import axios from 'axios';
+import { BiLeftArrow } from 'react-icons/bi';
+import { BiRightArrow } from 'react-icons/bi';
 
 /** APIURL */
 const APIURL = 'http://localhost:5000';
 
-function DealData(detaillist: DetailPropsType) {
-  const dealRelist = detaillist.detailRelist.Deal;
+function DealData({ detailRelist, detailid, detailtype }: DetailPropsType) {
+  const dealRelist = detailRelist.Deal;
   const [dealChartlist, setDealChartlist] = useState<any>();
 
   useEffect(() => {
     axios({
       method: 'get',
-      // url: APIURL + `/api/place/${props.type}/${props.id}`,
-      url: APIURL + `/api/place/APT/chart/9`,
+      url: APIURL + `/api/place/${detailtype}/chart/${detailid}`,
     })
       .then((response) => {
         console.log('데이터 전송 성공이구아나');
-        console.log(response.data);
+        console.log(response.data, '여기 확인부탁');
         setDealChartlist(response.data);
       })
       .catch((error) => {
@@ -91,6 +31,31 @@ function DealData(detaillist: DetailPropsType) {
         console.error(error);
       });
   }, []);
+
+  // 페이지당 아이템 수
+  const itemsPerPage = 10;
+  // 현재 페이지
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  // 시작 인덱스
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  // 끝 인덱스
+  const endIndex = currentPage * itemsPerPage;
+
+  // 페이지 변경 핸들러
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // 총 아이템 수
+  const totalItems = dealRelist.reduce((total, data) => {
+    return total + data.deals.length;
+  }, 0);
+
+  // 전체 페이지 수
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  // 현재 페이지의 아이템 목록
+  const currentPageItems = dealRelist.flatMap((data) => data.deals).slice(startIndex, endIndex);
 
   return (
     <div className={styles.component}>
@@ -104,15 +69,21 @@ function DealData(detaillist: DetailPropsType) {
         <div className={styles.floor}>층</div>
         <div className={styles.cost}>금액 (만원)</div>
       </div>
+
+      {currentPage > 1 && (
+        <button name="left" className={styles['left-btn']} onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+          <BiLeftArrow />
+        </button>
+      )}
+      {currentPage < totalPages && (
+        <button name="right" className={styles['right-btn']} onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+          <BiRightArrow />
+        </button>
+      )}
       <div>
-        {dealRelist &&
-          dealRelist.map((data: any, index: number) => (
-            <div key={index}>
-              {data.deals.map((item: any, index: number) => (
-                <DealDataItem key={index} dealDataItem={item} />
-              ))}
-            </div>
-          ))}
+        {currentPageItems.map((item: any, index: number) => (
+          <DealDataItem key={index} dealDataItem={item} />
+        ))}
       </div>
     </div>
   );
--- frontend/src/containers/Detail/DetailContainer.tsx
+++ frontend/src/containers/Detail/DetailContainer.tsx
@@ -1,269 +1,3 @@
-// import React, { useEffect, useRef, useState } from 'react';
-// import { Map } from 'react-kakao-maps-sdk';
-// import { useNavigate } from 'react-router-dom';
-// import DetailInformation, { DetailType } from '../../components/DetailInformation/DetailInformation';
-// import styles from './DetailContainer.module.scss';
-// import InfraIconamenities from '../../components/InfraIcon/InfraIconamenities';
-// import InfraIconcultures from '../../components/InfraIcon/InfraIconcultures';
-// import InfraIconschools from '../../components/InfraIcon/InfraIconschools';
-// import InfraIconsecurities from '../../components/InfraIcon/InfraIconsecurities';
-// import InfraIcontransports from '../../components/InfraIcon/InfraIcontransports';
-// import axios from 'axios';
-// import icon from '../../assets/icon.png';
-
-// import { useAppDispatch, useAppSelector } from '../../store/hooks';
-// import { setdetailContainerState } from '../../store/slices/detailContainerSlice';
-
-// /** 건물 타입과 건물 id */
-// type Props = {};
-
-// /** APIURL */
-// const APIURL = 'http://localhost:5000';
-
-// function Detailcontainer(props: Props) {
-//   const mapRef = useRef<HTMLDivElement>(null);
-//   const [map, setMap] = useState<any>(null);
-//   const [clusterer, setClusterer] = useState<any>(null);
-
-//   const dispatch = useAppDispatch();
-//   /** 매물 상세 정보 데이터 */
-//   const [detailRelist, setDetailRelist] = useState<DetailType>();
-
-//   /** 매물 상세 정보 요청 */
-//   useEffect(() => {
-//     axios({
-//       method: 'get',
-//       // url: APIURL + `/api/place/${props.type}/${props.id}`,
-//       url: APIURL + `/api/place/APT/2`,
-//     })
-//       .then((response) => {
-//         setDetailRelist(response.data);
-//       })
-//       .catch((error) => {
-//         // console.error('데이터 전송 실패이냐옹');
-//         console.error(error);
-//       });
-//   }, []);
-
-//   const detailContainer = useAppSelector((state) => state.detailContainerSlice.detailContainer);
-
-//   /** 매물 위치를 기반으로 지도 생성 */
-//   const createMap = async () => {
-//     if (window.kakao && window.kakao.maps && detailRelist) {
-//       let lat;
-//       let lng;
-
-//       try {
-//         lat = detailRelist.home.lat;
-//         lng = detailRelist.home.lng;
-
-//         const options = {
-//           center: new window.kakao.maps.LatLng(lat, lng),
-//           level: 4,
-//         };
-
-//         if (mapRef.current) {
-//           /** map */
-//           const map = new window.kakao.maps.Map(mapRef.current, options);
-
-//           /** clusterer */
-//           const clusterer = new kakao.maps.MarkerClusterer({
-//             map,
-//             averageCenter: true,
-//             minLevel: 5,
-//           });
-
-//           /** 바깥에서 map 쓸 수 있게 */
-//           setMap(map);
-
-//           /** 바깥에서 clusterer 쓸 수 있게 */
-//           setClusterer(clusterer);
-
-//           /** 현재 영역 가져옴 */
-//           const bounds = map.getBounds();
-
-//           /** 현재 영역의 매물들 가져옴 */
-//         }
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     }
-//   };
-
-//   /** 맵 생성 useEffect */
-//   useEffect(() => {
-//     createMap();
-//   }, [detailRelist]);
-
-//   const [dataList, setDataList] = useState<{ btnName: { id: number; name: string; lat: string; lng: string }[] }>({ btnName: [] });
-
-//   /** 현재 영역 매물 요청 */
-//   const handleBtnClick = (btnName: string) => {
-//     /** 기존 맵, 클러스터러 초기화 */
-//     clusterer.clear();
-//     if (
-//       btnName === '어린이집' ||
-//       btnName === '유치원' ||
-//       btnName === '초등학교' ||
-//       btnName === '중학교' ||
-//       btnName === '고등학교' ||
-//       btnName === '특수학교' ||
-//       btnName === '입시.검정 및 보습' ||
-//       btnName === '예체능'
-//     ) {
-//       /** 백엔드에 인프라 데이터 요청 */
-//       axios({
-//         method: 'get',
-//         // url: APIURL + `/api/place/${props.type}/${detailRelist.home.id}/${btnName}`,
-//         url: APIURL + `/api/place/APT/${detailRelist?.home.id}/school`,
-//       })
-//         .then((response) => {
-//           console.log('데이터 전송 성공이다멍멍 학군임');
-//           // console.log(response.data);
-
-//           if (btnName === '어린이집' || btnName === '유치원' || btnName === '초등학교' || btnName === '중학교' || btnName === '고등학교' || btnName === '특수학교') {
-//             response.data.school.map((item: any) => {
-//               if (item.type === btnName) {
-//                 setDataList(item.schools);
-//                 console.log(item.schools);
-//                 /** 마커 이미지 */
-//                 const markerImage = new kakao.maps.MarkerImage(
-//                   icon, // 이미지 경로
-//                   new kakao.maps.Size(30, 30), // 이미지 크기
-//                 );
-//                 /** api 요청으로 가져온 마커 리스트 */
-//                 let markers = item.schools.map((data: any) => ({
-//                   // Computed property name syntax
-//                   latlng: new window.kakao.maps.LatLng(data.lat, data.lng),
-//                 }));
-//                 /** 마커 표시 */
-//                 for (let i = 0; i < markers.length; i++) {
-//                   const marker = new kakao.maps.Marker({
-//                     map,
-//                     position: markers[i].latlng,
-//                     image: markerImage,
-//                   });
-
-//                   clusterer.addMarker(marker);
-//                 }
-//                 return;
-//               }
-//             });
-//           } else {
-//             response.data.academy.map((item: any) => {
-//               if (item.type === btnName) {
-//                 console.log(item.academys);
-//                 setDataList(item.academys);
-
-//                 /** 마커 이미지 */
-//                 const markerImage = new kakao.maps.MarkerImage(
-//                   icon, // 이미지 경로
-//                   new kakao.maps.Size(30, 30), // 이미지 크기
-//                 );
-//                 /** api 요청으로 가져온 마커 리스트 */
-//                 let markers = item.academys.map((data: any) => ({
-//                   // Computed property name syntax
-
-//                   latlng: new window.kakao.maps.LatLng(data.lng, data.type),
-//                 }));
-//                 /** 마커 표시 */
-//                 for (let i = 0; i < markers.length; i++) {
-//                   const marker = new kakao.maps.Marker({
-//                     map,
-//                     position: markers[i].latlng,
-//                     image: markerImage,
-//                   });
-
-//                   clusterer.addMarker(marker);
-//                 }
-//                 return;
-//               }
-//             });
-//           }
-//         })
-//         .catch((error) => {
-//           console.error('데이터 전송 실패다멍멍 학군임');
-//           // console.error(error);
-//         });
-//     } else {
-//       /** 백엔드에 인프라 데이터 요청 */
-//       axios({
-//         method: 'get',
-//         // url: APIURL + `/api/place/${props.type}/${detailRelist.home.id}/${btnName}`,
-//         url: APIURL + `/api/place/APT/${detailRelist?.home.id}/${btnName}`,
-//       })
-//         .then((response) => {
-//           console.log('데이터 전송 성공이다멍멍 학군아님');
-//           console.log(response.data);
-//           setDataList(response.data);
-//           /** 마커 이미지 */
-//           const markerImage = new kakao.maps.MarkerImage(
-//             icon, // 이미지 경로
-//             new kakao.maps.Size(30, 30), // 이미지 크기
-//           );
-//           /** api 요청으로 가져온 마커 리스트 */
-//           console.log(btnName);
-//           let markers = response.data[btnName].map((data: any) => ({
-//             // Computed property name syntax
-//             latlng: new window.kakao.maps.LatLng(data.lat, data.lng),
-//           }));
-//           /** 마커 표시 */
-//           for (let i = 0; i < markers.length; i++) {
-//             const marker = new kakao.maps.Marker({
-//               map,
-//               position: markers[i].latlng,
-//               image: markerImage,
-//             });
-
-//             clusterer.addMarker(marker);
-//           }
-//         })
-//         .catch((error) => {
-//           console.error('데이터 전송 실패다멍멍 학군아님');
-//           console.error(error);
-//         });
-//     }
-//   };
-//   const [tabIndex, setTabIndex] = useState(0);
-
-//   const onClickTab = (index: number) => {
-//     setTabIndex(index);
-//   };
-
-//   return (
-//     <div className={styles.container}>
-//       <div ref={mapRef} className={styles.map} />
-//       <div className={styles.icon}>
-//         <div className={styles.tab}>
-//           <div onClick={() => onClickTab(0)} className={tabIndex === 0 ? styles.selected : styles['not-selected']}>
-//             학군
-//           </div>
-//           <div onClick={() => onClickTab(1)} className={tabIndex === 1 ? styles.selected : styles['not-selected']}>
-//             교통
-//           </div>
-//           <div onClick={() => onClickTab(2)} className={tabIndex === 2 ? styles.selected : styles['not-selected']}>
-//             편의
-//           </div>
-//           <div onClick={() => onClickTab(3)} className={tabIndex === 3 ? styles.selected : styles['not-selected']}>
-//             치안
-//           </div>
-//           <div onClick={() => onClickTab(4)} className={tabIndex === 4 ? styles.selected : styles['not-selected']}>
-//             문화
-//           </div>
-//         </div>
-//         {tabIndex === 0 && <InfraIconschools selectedBtn={''} setSelectedBtn={handleBtnClick} />}
-//         {tabIndex === 1 && <InfraIcontransports selectedBtn={''} setSelectedBtn={handleBtnClick} />}
-//         {tabIndex === 2 && <InfraIconamenities selectedBtn={''} setSelectedBtn={handleBtnClick} />}
-//         {tabIndex === 3 && <InfraIconsecurities selectedBtn={''} setSelectedBtn={handleBtnClick} />}
-//         {tabIndex === 4 && <InfraIconcultures selectedBtn={''} setSelectedBtn={handleBtnClick} />}
-//       </div>
-//       {detailRelist ? <DetailInformation detailRelist={detailRelist} /> : <></>}
-//     </div>
-//   );
-// }
-
-// export default Detailcontainer;
-
 import React, { useEffect, useRef, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 import { useNavigate } from 'react-router-dom';
@@ -296,6 +30,8 @@ function Detailcontainer(props: DetailContainerProps) {
 
   /** 매물 상세 정보 요청 */
   useEffect(() => {
+    console.log(props.detailtype, '나오냐?');
+
     axios({
       method: 'get',
       url: APIURL + `/api/place/${props.detailtype}/${props.detailid}`,
@@ -377,7 +113,7 @@ function Detailcontainer(props: DetailContainerProps) {
       axios({
         method: 'get',
         // url: APIURL + `/api/place/${props.type}/${detailRelist.home.id}/${btnName}`,
-        url: APIURL + `/api/place/APT/${detailRelist?.home.id}/school`,
+        url: APIURL + `/api/place/${props.detailtype}/${detailRelist?.home.id}/school`,
       })
         .then((response) => {
           console.log('데이터 전송 성공이다멍멍 학군임');
@@ -452,7 +188,7 @@ function Detailcontainer(props: DetailContainerProps) {
       axios({
         method: 'get',
         // url: APIURL + `/api/place/${props.type}/${detailRelist.home.id}/${btnName}`,
-        url: APIURL + `/api/place/APT/${detailRelist?.home.id}/${btnName}`,
+        url: APIURL + `/api/place/${props.detailtype}/${detailRelist?.home.id}/${btnName}`,
       })
         .then((response) => {
           console.log('데이터 전송 성공이다멍멍 학군아님');
@@ -519,7 +255,7 @@ function Detailcontainer(props: DetailContainerProps) {
         {tabIndex === 3 && <InfraIconsecurities selectedBtn={''} setSelectedBtn={handleBtnClick} />}
         {tabIndex === 4 && <InfraIconcultures selectedBtn={''} setSelectedBtn={handleBtnClick} />}
       </div>
-      {detailRelist ? <DetailInformation detailRelist={detailRelist} /> : <></>}
+      {detailRelist ? <DetailInformation detailRelist={detailRelist} detailid={props.detailid} detailtype={props.detailtype} /> : <></>}
     </div>
   );
 }
