48b1d00bbcc914cb64e3c3df37878f6dba2b7970
cmzlsho@gmail.com <cmzlsho@gmail.com>
2023-04-06 17:44:09 +0900

    ✨최근 본 뉴스 목업

--- backend/src/main/java/com/iuj/backend/api/controller/NewsController.java
+++ backend/src/main/java/com/iuj/backend/api/controller/NewsController.java
@@ -4,10 +4,12 @@ import com.iuj.backend.api.domain.enums.ErrorCode;
 import com.iuj.backend.api.exception.CustomException;
 import com.iuj.backend.api.service.NewsService;
 import io.swagger.annotations.ApiOperation;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
@@ -29,6 +31,17 @@ public class NewsController {
             }
         } catch (Exception e){
             throw new CustomException(ErrorCode.NOTFOUND_NEWS);
+        }
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "최근 본 뉴스 저장을 위한 api")
+    public ResponseEntity<Object> setRecentViewNews(@PathVariable Long id){
+        try{
+            System.out.println(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (Exception e){
+            throw new CustomException(ErrorCode.UNAUTHORIZED);
         }
     }
 }
