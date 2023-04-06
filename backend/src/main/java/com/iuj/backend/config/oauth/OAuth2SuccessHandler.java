4b542964080a5fc93d922174391497832135572c
mauv2sky <mauv2sky@gmail.com>
2023-04-06 11:34:35 +0900

    🐛 토큰 리다이렉트 경로 변경

--- backend/src/main/java/com/iuj/backend/config/oauth/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/iuj/backend/config/oauth/OAuth2SuccessHandler.java
@@ -82,7 +82,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
             redirectStrategy.sendRedirect(request, response, targetUrl);
         } else { // 소셜로그인 요청일 경우
-            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:5173")
+            String targetUrl = UriComponentsBuilder.fromUriString("https://j8e103.p.ssafy.io")
                     .queryParam("access_token", tokenDto.getAccessToken())
                     .queryParam("refresh_token", tokenDto.getRefreshToken())
                     .queryParam("expiration_date", tokenDto.getAccessTokenExpiresIn())
